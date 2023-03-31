2f8118b946993e874297833bc67c7329dabdf8c4
XD_CZ <mandlemankiller@email.cz>
2023-03-31 21:19:17 +0200

    Add reload warning

--- src/main/java/cz/jeme/programu/hibernator/HibernationSchedule.java
+++ src/main/java/cz/jeme/programu/hibernator/HibernationSchedule.java
@@ -1,6 +1,5 @@
 package cz.jeme.programu.hibernator;
 
-import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class HibernationSchedule extends BukkitRunnable {
@@ -13,7 +12,6 @@ public class HibernationSchedule extends BukkitRunnable {
 
     @Override
     public void run() {
-        Bukkit.getLogger();
         snoreHandler.enableHibernation();
     }
 }
